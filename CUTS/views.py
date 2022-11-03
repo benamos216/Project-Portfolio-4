a2b6a4ee08770e5b59029ac8bf90a82cb4f55878
benamos216 <benamos216@gmail.com>
2022-11-03 12:51:56 +0000

    set first view to load main page, and get suppliers

--- CUTS/views.py
+++ CUTS/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Supplier
 
 # Create your views here.
+
+
+def get_supplier(request):
+    suppliers = Supplier.objects.all()
+    context = {
+        'suppliers': suppliers
+    }
+    return render(request, "supplier.html", context)
--- QC_CUTS/urls.py
+++ QC_CUTS/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from CUTS import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.get_supplier, name='get_supplier'),
 ]
