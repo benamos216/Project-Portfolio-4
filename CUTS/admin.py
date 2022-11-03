591d870c407ca79ef6fec1ea07870086f18c7529
benamos216 <benamos216@gmail.com>
2022-11-03 12:43:27 +0000

    created models

--- CUTS/admin.py
+++ CUTS/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Supplier, Range, Roll
 
 # Register your models here.
+admin.site.register(Supplier)
+admin.site.register(Range)
+admin.site.register(Roll)
--- CUTS/models.py
+++ CUTS/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Supplier (models.Model):
+    supplier = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.supplier
+
+
+class Range (models.Model):
+    supplier = models.ForeignKey('Supplier', related_name='Supplier', on_delete=models.CASCADE)
+    ranges = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.ranges
+
+
+class Roll (models.Model):
+    ranges = models.ForeignKey('Range', on_delete=models.CASCADE, related_name='Range')
+    roll_colour = models.CharField(max_length=20, null=False, blank=False)
+    roll_width = models.CharField(max_length=2, null=False, blank=False)
+    roll_size = models.DecimalField(max_digits=4, decimal_places=2, null=False, blank=False)
+    location = models.CharField(max_length=3, null=False, blank=False)
+
+    def __str__(self):
+        return self.rolls
