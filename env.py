af71f9f69d2808e86a2e92ab21677246c4e765ec
benamos216 <benamos216@gmail.com>
2022-12-05 18:12:46 +0000

    deployment issue

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://kznrgnbo:nGEABKrwOwnqMAg39LsqZfD8NROpnkem@surus.db.elephantsql.com/kznrgnbo'
+os.environ['SECRET_KEY'] = 'o87m*$lawp%9fkf=9*z@w67y+7f+8ccqt8(d5s0=1a^-dytm$s'
+os.environ['CLOUDINARY_URL'] = 'cloudinary://542198291661993:F2j7DzGGfZNIvK-65xEE4CEguNQ@dw88orkqf'
